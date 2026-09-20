bb16c280548bb1e63f394d56a2c6d5ae99264bbf
salpmusic <salpmusic@users.noreply.github.com>
2026-09-20 17:08:11 +0900

    Raise AGP/compileSdk for GeckoView 154 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 android {
     namespace = "com.salp.mobilepc"
-    compileSdk = 35
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.salp.mobilepc"
         minSdk = 26
